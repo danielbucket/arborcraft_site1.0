5018b1fa220390063a4e3ae6b4cc98216ca86244
Daniel Bucket <danieljbucket@gmail.com>
2023-11-03 18:45:11 -0600

    ended up rewriting pretty much everything for the SlideShow component. Will terminate the branch and make further edits an a new branch with an accurate name

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -28,41 +28,44 @@ const SlideShow = () => {
 
 	const aboutUsBody = () => {
 		const container = document.createElement('div');
-			container.classList.add('about-us-body', 'slide-card-body');
+			container.classList.add('about-us-body-container', 'topic-card-body');
 			container.innerHTML = "We're not your typical do it fast and do it dirty tree care company. At ArborCraft we take the time to do the job thoroughly and do it well. Our staff is comprised of educated and experienced individuals who know the importance of safety for themselves as tree workers as well as the safety for you and your property. We base our reputation on doing the job right the first time.";
 		return container;
 	};
 
 	const servicesBody = () => {
 		const container = document.createElement('div');
-			container.classList.add('services-body', 'slide-card-body');
+			container.classList.add('services-body-container', 'topic-card-body');
 			container.innerHTML = "Tree Pruning, Trimming, Removals, Planting, Nutrition and Care";
 		return container;
 	};
 
 	const portfolioBody = () => {
 		const container = document.createElement('div');
-			container.classList.add('portfolio-body', 'slide-card-body');
+			container.classList.add('portfolio-body-container', 'topic-card-body');
 			container.innerHTML = "Portfolio";
 		return container;
 	};
 
 	const resourcesBody = () => {
 		const container = document.createElement('div');
-			container.classList.add('resources-body','slide-card-body');
+			container.classList.add('resources-body-container','topic-card-body');
 			container.innerHTML = "Resources";
 		return container;
 	};
 
+
+
+
 	const reviewsBody = (data) => {
 		const reviewsBodyContainer = document.createElement('div');
-			reviewsBodyContainer.classList.add('slide-card-body', 'reviews-body-container');
+			reviewsBodyContainer.classList.add('reviews-body-container');
 
 		const reviewCardsArr = data.map((i,num) => {
 			const { review, author } = i;
-			const cardContainer = document.createElement('div');
-				cardContainer.classList.add('review-card-container');
-				cardContainer.id = num;
+			const reviewCardContainer = document.createElement('div');
+				reviewCardContainer.classList.add('review-card-container');
+				reviewCardContainer.id = num;
 
 			const reviewText = document.createElement('div');
 				reviewText.classList.add('review-text');
@@ -70,23 +73,26 @@ const SlideShow = () => {
 
 			const authorText = document.createElement('div');
 				authorText.classList.add('author-text');
-				authorText.innerHTML = author;
+				authorText.innerHTML = "-" + author;
 
-			cardContainer.appendChild(reviewText);
-			cardContainer.appendChild(authorText);
-			return cardContainer;
+			reviewCardContainer.appendChild(reviewText);
+			reviewCardContainer.appendChild(authorText);
+
+			return reviewCardContainer;
 		});
+
+
 
 		reviewCardsArr.map(i => reviewsBodyContainer.appendChild(i));
 		return reviewsBodyContainer;
 	};// end reviewsBody() returns a single parent element with childnodes
 
 	const slideData = [
-		// {
-		// 	slideName: "service-area",
-		// 	headerText: "Serving Golden, Colorado, \<br>and Denver's western 'burbs",
-		// 	cardElement: MapContainer(),
-		// },
+		{
+			slideName: "service-area",
+			headerText: "Serving Golden, Colorado, \<br>and Denver's western 'burbs",
+			cardElement: MapContainer(),
+		},
 		{
 			slideName: "about-us",
 			headerText: "About Us",
@@ -117,8 +123,8 @@ const SlideShow = () => {
 	const slideshow = slideData.map(topic => {
 		const { slideName, cardElement, headerText } = topic;
 
-		const slidesContainer = document.createElement('div');
-			slidesContainer.classList.add('slides-container', slideName + "-container");
+		const topicContainer = document.createElement('div');
+			topicContainer.classList.add('slide-topic-container', slideName + "-container");
 
 		const topicHeader = document.createElement('div');
 			topicHeader.classList.add('topic-header');
@@ -127,13 +133,13 @@ const SlideShow = () => {
 			topicHeader.appendChild(swipeBtnRight());
 
 		const topicBody = document.createElement('div');
-			topicBody.classList.add('topic-body');
+			topicBody.classList.add('topic-card-body');
 			topicBody.appendChild(cardElement);
 
-		slidesContainer.appendChild(topicHeader);
-		slidesContainer.appendChild(topicBody);
+		topicContainer.appendChild(topicHeader);
+		topicContainer.appendChild(topicBody);
 
-		return slidesContainer;
+		return topicContainer;
 	}); // end booty. returns an array of cards
 
 	slideshow.map(i => slideShowContainer.appendChild(i));
--- src/components/SlideShow/reviewsStub.js
+++ src/components/SlideShow/reviewsStub.js
@@ -7,20 +7,20 @@ export const reviewsStubArr = [
 		review: "The quality of work is as good as I expected",
 		author: "Davey Beans",
 	},
-	{
-		review: "Prices were fair and comparable, mmkay",
-		author: "Mr. Mackey"
-	},
-	{
-		review: "I didn't know that so much wood would come out out the canopy? My tree looks great!",
-		author: "Jennifer Coolidge",
-	},
+	// {
+	// 	review: "Prices were fair and comparable, mmkay?",
+	// 	author: "Mr. Mackey"
+	// },
+	// {
+	// 	review: "I didn't know that so much wood would come out out the canopy? My tree looks great!",
+	// 	author: "Jennifer Coolidge",
+	// },
 	{
 		review: "The tree injections he gave my Linden tree made the canopy look great this summer!",
 		author: "Gregg",
 	},
 	{
 		review: "They cleaned up very thoroughly after.",
-		author: "Megan Fox",
+		author: "Morgan FreePerson",
 	},
 ];
